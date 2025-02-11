363bed238979996b53b5fc4ded2b456c57725bdc
xh3b4sd <xh3b4sd@gmail.com>
2025-02-11 11:44:02 +0100

    ensure limited range of motion for vector header target (#35)

---
+++ pkg/vector/adjust.go
@@ -0,0 +1,14 @@
+package vector
+
+func (v *Vector) Adjust(del int) byte {
+	// TODO increment or decrement size based on the given delta
+	// TODO update body part radius
+	// TODO update motion range angle
+	// TODO update number of body parts
+
+	// TODO return 0 for no change in body parts
+	// TODO return 1 for adding a body part
+	// TODO return 2 for removing a body part
+
+	return 0x0
+}
